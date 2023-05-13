624f7855818c82a76a62f3022b7c1241be9b7c7a
nb-mk2 <121810779+nb-mk2@users.noreply.github.com>
2023-05-13 16:08:16 -0300

    Add files via upload

---
+++ buscar.php
@@ -0,0 +1,140 @@
+<?php
+/*
+* Script: Cargar datos de lado del servidor con PHP y MySQL
+* Autor: Marco Robles
+* Team: Códigos de Programación
+*/
+
+
+$conn = new mysqli("localhost", "root", "", "pos");
+
+/* Comprobando si hay un error de conexión. */
+if ($conn->connect_error) {
+    die('Error de conexion ' . $conn->connect_error); }
+
+/* Un arreglo de las columnas a mostrar en la tabla */
+$columns = ['product_id', 'product_code', 'price'];
+
+/* Nombre de la tabla */
+$table = "products";
+
+$id = 'product_id';
+
+$campo = isset($_POST['campo']) ? $conn->real_escape_string($_POST['campo']) : null;
+
+
+/* Filtrado */
+$where = '';
+
+if ($campo != null) {
+    $where = "WHERE (";
+
+    $cont = count($columns);
+    for ($i = 0; $i < $cont; $i++) {
+        $where .= $columns[$i] . " LIKE '%" . $campo . "%' OR ";
+    }
+    $where = substr_replace($where, "", -3);
+    $where .= ")";
+}
+
+/* Limit */
+$limit = isset($_POST['registros']) ? $conn->real_escape_string($_POST['registros']) : 10;
+$pagina = isset($_POST['pagina']) ? $conn->real_escape_string($_POST['pagina']) : 0;
+
+if (!$pagina) {
+    $inicio = 0;
+    $pagina = 1;
+} else {
+    $inicio = ($pagina - 1) * $limit;
+}
+
+$sLimit = "LIMIT $inicio , $limit";
+
+/**
+ * Ordenamiento
+ */
+
+ $sOrder = "";
+ if(isset($_POST['orderCol'])){
+    $orderCol = $_POST['orderCol'];
+    $oderType = isset($_POST['orderType']) ? $_POST['orderType'] : 'asc';
+    
+    $sOrder = "ORDER BY ". $columns[intval($orderCol)] . ' ' . $oderType;
+ }
+
+
+/* Consulta */
+$sql = "SELECT SQL_CALC_FOUND_ROWS " . implode(", ", $columns) . "
+FROM $table
+$where
+$sOrder
+$sLimit";
+$resultado = $conn->query($sql);
+$num_rows = $resultado->num_rows;
+
+/* Consulta para total de registro filtrados */
+$sqlFiltro = "SELECT FOUND_ROWS()";
+$resFiltro = $conn->query($sqlFiltro);
+$row_filtro = $resFiltro->fetch_array();
+$totalFiltro = $row_filtro[0];
+
+/* Consulta para total de registro filtrados */
+$sqlTotal = "SELECT count($id) FROM $table ";
+$resTotal = $conn->query($sqlTotal);
+$row_total = $resTotal->fetch_array();
+$totalRegistros = $row_total[0];
+
+/* Mostrado resultados */
+$output = [];
+$output['totalRegistros'] = $totalRegistros;
+$output['totalFiltro'] = $totalFiltro;
+$output['data'] = '';
+$output['paginacion'] = '';
+
+if ($num_rows > 0) {
+    while ($row = $resultado->fetch_assoc()) {
+        $output['data'] .= '<tr>';
+        $output['data'] .= '<td>' . $row['product_id'] . '</td>';
+        $output['data'] .= '<td>' . $row['product_code'] . '</td>'; 
+        $output['data'] .= '<td>' . $row['price'] . '</td>';
+        $output['data'] .= '<td ><a id="relface" rel="facebox" title="Click para editar materiales" href="editproduct.php?id=' . $row['product_id'] . '"><button class="btn btn-warning"><i class="icon-edit"></i> </button> </a>
+        ';
+        $output['data'] .= '</tr>';
+    }
+} else {
+    $output['data'] .= '<tr>';
+    $output['data'] .= '<td colspan="7">Sin resultados</td>';
+    $output['data'] .= '</tr>';
+}
+
+if ($output['totalRegistros'] > 0) {
+    $totalPaginas = ceil($output['totalRegistros'] / $limit);
+
+    $output['paginacion'] .= '<nav>';
+    $output['paginacion'] .= '<ul class="pagination">';
+
+    $numeroInicio = 1;
+
+    if(($pagina - 4) > 1){
+        $numeroInicio = $pagina - 4;
+    }
+
+    $numeroFin = $numeroInicio + 9;
+
+    if($numeroFin > $totalPaginas){
+        $numeroFin = $totalPaginas;
+    }
+
+    for ($i = $numeroInicio; $i <= $numeroFin; $i++) {
+        if ($pagina == $i) {
+            $output['paginacion'] .= '<li class="page-item active"><a class="page-link" href="#">' . $i . '</a></li>';
+        } else {
+            $output['paginacion'] .= '<li class="page-item"><a class="page-link" href="#" onclick="nextPage(' . $i . ')">' . $i . '</a></li>';
+        }
+    }
+
+    $output['paginacion'] .= '</ul>';
+    $output['paginacion'] .= '</nav>';
+}
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
